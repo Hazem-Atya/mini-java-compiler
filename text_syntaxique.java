16dcb350c529a8e122f1789d05a6c65c15160a50
Hazem-Atya <hazematya1@gmail.com>
2022-05-10 19:10:53 +0100

    adding test files

--- text_syntaxique.java
+++ text_syntaxique.java
@@ -1,26 +1,28 @@
-class MAIN{
-public static void main( String [] args ){ // args 
-
+class  MAIN{
+public static  void main( String [] args )// args  
+{
 int a;
 int b;
 a=5;
 }
 }
-class Test { // open braces 
-public  float   m(int a) // return type 
+class  Test { // open braces + key word class 
+public int   m(int a) // return type 
 {
 int [] a; // une des deux accolades
 int b;
+int i;
+int t;
 {a=5;}
 a=5;
 
 System.out.println(a); // la parenthèse fermante
-b = 5 ;//  affectation,  semicolon
+b=  5 ;//  affectation,  semicolon
 //b=5;
-//if  (a)
+
 if (!(a && b))
 {
-	a=5;
+	a=7;
 }else 
 	b=4;
 
@@ -29,12 +31,10 @@ while (i &&  t) //  "&&"
 a=5;
 }
 
-return a; //  return 
+return 5;
 
 }
 
 }
-
-
 
 
